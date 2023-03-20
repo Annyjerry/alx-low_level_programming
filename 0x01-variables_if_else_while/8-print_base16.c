761535b71bfa114947cfda4b485694bda375462e
Annyjerry <aniebietlawrence191@gmail.com>
2023-03-20 12:29:18 -0700

    digit in base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints all the numbers of base 16 in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+	char ch = 'a';
+
+	if (num != 9)
+	{
+		putchar('0' + num);
+		num++;
+	}
+	if (ch != 'f')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
