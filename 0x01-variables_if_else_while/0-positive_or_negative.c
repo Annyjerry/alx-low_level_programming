ccb6005338f22a5826ca2376e27e2a2d041560c5
Annyjerry <aniebietlawrence191@gmail.com>
2023-03-20 01:17:53 -0700

    Random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,11 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+/**
+ * main - main function to generate a random number
+ * Return: Always 0 (Success)
+ */
 
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
@@ -11,15 +14,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 if (n < 0)
 {
-printf("%d is nagative \n", n);
+	printf("%d is nagative \n", n);
 }
 else if (n == 0)
 {
-printf("%d is zero \n", n);
+	printf("%d is zero \n", n);
 }
-else
+else if(n > 0)
 {
-printf("%d is positive \n", n);
+	printf("%d is positive \n", n);
 }
-	return (0);
+return (0);
 }
