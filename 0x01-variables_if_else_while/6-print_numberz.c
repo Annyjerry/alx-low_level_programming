4984a3a279b1f1ef30c1b0b0451537b6826fba73
Annyjerry <aniebietlawrence191@gmail.com>
2023-03-20 11:55:28 -0700

    digits of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - A program to print digit of base 10
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
