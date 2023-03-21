ce201bc629ebea3e9a502d6e3e493121196040ea
Annyjerry <aniebietlawrence191@gmail.com>
2023-03-21 11:56:01 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,16 +7,16 @@
 
 void print_alphabet_x10(void)
 {
-	char ch = 'a';
-	int i;
+	char ch;
+	int i = 0;
 
-	for (i = 1; i < 11; i++)
+	while (i < 10)
 	{
-		while (ch <= 'z')
+		for (ch = 'a'; ch <= 'z'; ch++)
 		{
 			_putchar(ch);
-			ch++;
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
+	i++;
 }
